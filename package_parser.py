e450e7fee0a9d73699141cf5423e67d0a759b0ae
NamelessCrab <savin493ss@gmail.com>
2026-06-11 13:23:55 +0800

    v0.5

--- package_parser.py
+++ package_parser.py
@@ -10,5 +10,7 @@ for line in apt_result.stdout.splitlines()[1:]:
     if '/' in line and 'python3' in line:
         name = line.split('/')[0]
         version = line.split('/')[1]
+        version = version.split(' ')[1]
+        version = version.split(' ')[0]
         print(name, version)
     
