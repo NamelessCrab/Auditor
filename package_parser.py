80ff8ee146186b19ead24645c1b478b307bc9cb6
NamelessCrab <savin493ss@gmail.com>
2026-06-11 13:11:31 +0800

    Добавил поиск строки с питоном и вывод потенциального id

--- package_parser.py
+++ package_parser.py
@@ -7,7 +7,13 @@ apt_result = subprocess.run(
     )
 
 apt_lines = apt_result.stdout.splitlines()
-print(apt_lines)
+p_ids = []
+for line in apt_lines:
+    id = line.index('python3')
+    p_ids.append[apt_lines[id]]
+        
+print(p_ids)
+
 '''
 for line in result.stdout.splitlines():
     print(line)
