ee2e5a5e270b0bc77d402cade437b4833763fdde
NamelessCrab <savin493ss@gmail.com>
2026-06-24 11:12:50 +0800

    Окультурил файл

--- package_parser.py
+++ package_parser.py
@@ -2,25 +2,30 @@ import subprocess
 
 import pandas as pd
 
-apt_result = subprocess.run(
-    ['apt', 'list', '--installed'],
-    stdout=subprocess.PIPE,
-    text=True
-    )
-package_dict = {
-    'name': [],
-    'version': []
-}
-for line in apt_result.stdout.splitlines()[1:]:
-    if '/' in line:
-        package_dict['name'].append(line.split('/')[0])
-        version = line.split('/')[1]
-        version = version.split(' ')[1]
-        version = version.split(' ')[0]
-        package_dict['version'].append(version)
+def get_package_list() -> pd.DataFrame:
+    apt_result = subprocess.run(
+        ['apt', 'list', '--installed'],
+        stdout=subprocess.PIPE,
+        text=True
+        )
+    package_dict = {
+        'name': [],
+        'version': []
+    }   
+    for line in apt_result.stdout.splitlines()[1:]:
+        if '/' in line:
+            package_dict['name'].append(line.split('/')[0])
+            version = line.split('/')[1]
+            version = version.split(' ')[1]
+            version = version.split(' ')[0]
+            package_dict['version'].append(version)
+    return pd.DataFrame(package_dict)
 
-df = pd.DataFrame(package_dict)
-print(df[df['name'].str.contains('fox')])
     
         
 
+
+if __name__ == "__main__":
+    df = get_package_list()
+    test = df[df['name'].str.contains('fox')]
+    print(test['name'], '|')
