049aa0f644f40009efe8c754df4b74de9e17e5da
NamelessCrab <savin493ss@gmail.com>
2026-06-24 14:00:28 +0800

    v0.3

--- package_parser.py
+++ package_parser.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 import pandas as pd
 
@@ -23,11 +24,14 @@ def get_package_list() -> pd.DataFrame:
             version = version.split(' ')[1]
             version = version.split(' ')[0]
             package_dict['version'].append(version)
-    return pd.DataFrame(package_dict)
+
+    df = pd.DataFrame(package_dict)
+    df['version'] = df[df['version'].str.normalize_version()]
+    return df
 
 def get_dpkg_list() -> pd.DataFrame:
         dpkg_result = subprocess.run(
-        ['dpkg-query', '-W', "-f='${Package}\t${Version}\t${Architecture}\n'"],
+        ['dpkg-query', '-W', "-f='${Package}\t${Version}\n'"],
         stdout=subprocess.PIPE,
         text=True
     )
@@ -38,12 +42,21 @@ def load_version_list() -> pd.DataFrame:
     df["Название ПО"] = df["Название ПО"].str.lower()  
     return df 
 
-
+def normalize_version(version: str) -> str:
+    version = (version or "").strip()
+    v_match = re.match(r"^\s*(\d+)(?:\.(\d+))?(?:\.(\d+))?", version)
+    if not v_match:
+        return version
+    major, minor, patch = v_match.group(1), v_match.group(2), v_match.group(3)
+    if patch is not None:
+        return f"{major}.{minor}.{patch}"
+    if minor is not None:
+        return f"{major}.{minor}"
+    return major
 
 if __name__ == "__main__":
-    #df_pkg = get_package_list()
+    df_pkg = get_package_list()
     #df_lst = load_version_list()
-    #test = df_pkg[df_pkg['name'].str.contains('fox')]
-    #print(df_pkg[df_pkg['name'].str.contains('fox')])
+    
+    print(df_pkg.head())
     #print(df_lst[df_lst['Название ПО'].str.contains('fox')])
-    get_dpkg_list()
